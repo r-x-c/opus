490fa286a904cffca956271b045a32067405f652
rxc <rxc@umich.edu>
2018-01-30 16:05:00 -0800

    force test to work with python2 for now

--- git/hw2_python.py
+++ git/hw2_python.py
@@ -19,34 +19,34 @@ def group(seq):
     else:
         return (seq[:5],) if len(seq) == 5 else (seq[:5],) + group(seq[5:])  
 
-def make_accumulator():
-    """Return an accumulator function that takes a single numeric
-    argument and accumulates that argument into total, then
-    returns total.
+# def make_accumulator():
+#     """Return an accumulator function that takes a single numeric
+#     argument and accumulates that argument into total, then
+#     returns total.
 
-    >>> acc = make_accumulator()
-    >>> acc(15)
-    15
-    >>> acc(10)
-    25
-    >>> acc2 = make_accumulator()
-    >>> acc2(7)
-    7
-    >>> acc3 = acc2
-    >>> acc3(6)
-    13
-    >>> acc2(5)
-    18
-    >>> acc(4)
-    29
-    """
-    pass # replace with your solution
-    total = 0
-    def accumulate(val):
-        nonlocal total
-        total += val 
-        return total
-    return accumulate
+#     >>> acc = make_accumulator()
+#     >>> acc(15)
+#     15
+#     >>> acc(10)
+#     25
+#     >>> acc2 = make_accumulator()
+#     >>> acc2(7)
+#     7
+#     >>> acc3 = acc2
+#     >>> acc3(6)
+#     13
+#     >>> acc2(5)
+#     18
+#     >>> acc(4)
+#     29
+#     """
+#     pass # replace with your solution
+#     total = 0
+#     def accumulate(val):
+#         nonlocal total
+#         total += val 
+#         return total
+#     return accumulate
 
 class Timer:
     """A timer class that can be used as a context manager with
